f4e12d7613dd391926589c5b51071423d76ab451
Sahil Nare <sahilnare78@gmail.com>
2020-03-19 19:14:21 +0530

    Added static files

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 const express = require('express')
 const app = express()
-// const path = require('path')
+const path = require('path')
 const port = process.env.PORT || 3000
 // const router = express.Router()
 
@@ -9,7 +9,8 @@ const port = process.env.PORT || 3000
 // router.get('/',function(req,res){
 //   res.sendFile(path.join(__dirname+'/index.html'))
 // });
-app.use(express.static(__dirname + '/public'))
+app.use(express.static(path.join(__dirname + '/public')))
+// app.use(express.static(__dirname + '/public/js'))
 // app.use('/', router);
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
