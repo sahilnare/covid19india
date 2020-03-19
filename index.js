fc78216d71847c6a52ea733bf48fd809335a541b
Sahil Nare <sahilnare78@gmail.com>
2020-03-19 22:07:21 +0530

    Description box

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 3000
 // router.get('/',function(req,res){
 //   res.sendFile(path.join(__dirname+'/index.html'))
 // });
-app.use(express.static(path.join(__dirname + '/public')))
+app.use(express.static(path.join(__dirname + '/public/')))
 // app.use(express.static(__dirname + '/public/js'))
 // app.use('/', router);
 
