96b1d3011b4054388b47c22c6477c5ea26e3b177
Sahil Nare <sahilnare78@gmail.com>
2020-03-19 18:29:37 +0530

    Added express

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const express = require('express')
 const app = express()
 // const path = require('path')
-const port = 3000
+const port = process.env.PORT || 3000
 // const router = express.Router()
 
 // app.get('/', (req, res) => res.send('Hello World!'))
