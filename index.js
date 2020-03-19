f7869b86fe033b8be2ea5ab1c54233b9c5b6ab75
Sahil Nare <sahilnare78@gmail.com>
2020-03-19 18:22:38 +0530

    Did it

---
+++ index.js
@@ -0,0 +1,15 @@
+
+const express = require('express')
+const app = express()
+// const path = require('path')
+const port = 3000
+// const router = express.Router()
+
+// app.get('/', (req, res) => res.send('Hello World!'))
+// router.get('/',function(req,res){
+//   res.sendFile(path.join(__dirname+'/index.html'))
+// });
+app.use(express.static(__dirname + '/public'))
+// app.use('/', router);
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
